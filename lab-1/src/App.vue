624039914ab145bd5961f30851d39c074c3a9bc8
juandesousa <juan.cdsb@gmail.com>
2022-08-30 20:58:19 +0200

    done

---
+++ lab-1/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Vue 3!</h1>
+    <about></about>
+  </div>
+</template>
+
+<script>
+import About from "./components/About.vue";
+export default {
+  components: { About },
+  name: "App",
+};
+</script>
+
+<style></style>
